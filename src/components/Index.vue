ff0a71e7ea75c8366d875ed5d20f406580cf6594
945188363 <niujingheng1997@163.com>
2020-04-25 17:16:39 +0800

    首页基本样式渲染

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -9,57 +9,87 @@
     </el-header>
     <el-container>
       <!-- 侧边 -->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <!--收缩按钮-->
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!--侧边栏菜单区域-->
         <el-menu
-          default-active="2"
           class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
           background-color="#545c64"
           text-color="#fff"
-          active-text-color="#ffd04b">
-          <el-submenu index="1">
+          active-text-color="#ffd04b"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-location"></i>
+            <span>API管理</span>
+          </el-menu-item>
+
+          <el-submenu index="2">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>注册方式与负载</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
+            <el-menu-item index="2-1">服务注册方式</el-menu-item>
+            <el-menu-item index="2-2">负载均衡</el-menu-item>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
+
+          <el-menu-item index="3">
+            <i class="el-icon-location"></i>
+            <span>API管理</span>
           </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
+
           <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
+            <i class="el-icon-location"></i>
+            <span>API监控</span>
           </el-menu-item>
+
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>网关设置</span>
+            </template>
+            <el-menu-item index="5-1">日志设置</el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="6">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>插件设置</span>
+            </template>
+          </el-submenu>
         </el-menu>
       </el-aside>
       <!-- 主体 -->
-      <el-main>Main</el-main>
+      <el-main>
+        <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+          <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+          <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+          <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+        </el-tabs>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
 export default {
-  name: 'Index'
+  name: 'Index',
+  data: function () {
+    return {
+      isCollapse: false
+    }
+  },
+  // 存放 方法
+  methods: {
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    }
+  }
+
 }
 </script>
 
@@ -85,8 +115,20 @@ export default {
   }
   .el-aside{
     background-color: #545c64;
+    .el-menu{
+      border-right: 0;
+    }
   }
   .el-main{
     background-color: azure;
+  }
+  .toggle-button{
+    background-color: lightslategray;
+    font-size: 10px;
+    line-height: 24px;
+    color: white;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
   }
 </style>
