de8c568831a49977d7492674f9cf846d292126ad
945188363 <niujingheng1997@163.com>
2020-05-29 23:57:49 +0800

    api新增字段处理

--- src/components/api/ApiManager.vue
+++ src/components/api/ApiManager.vue
@@ -31,6 +31,18 @@
                     <p class="lh_15"><span >api名称&nbsp;:&nbsp;</span><br></p>
                     <el-input style="width: 300px;" v-model="ApiName" placeholder="请输入内容"></el-input>
                   </div>
+                  <!--api服务端协议-->
+                  <div style="margin-top: 20px;">
+                    <p class="lh_15"><span>服务端协议&nbsp;:&nbsp;</span><br></p>
+                    <el-select style="width: 300px;" v-model="ProtocolType" placeholder="请选择">
+                      <el-option
+                        v-for="item in ProtocolTypes"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                      </el-option>
+                    </el-select>
+                  </div>
                   <!--api转发地址-->
                   <div style="margin-top: 20px;">
                     <p class="lh_15">
@@ -84,6 +96,15 @@
                       </el-tooltip><br>
                     </p>
                     <el-input style="width: 300px;" v-model="ApiTimeout" placeholder="请输入内容"></el-input>
+                  </div>
+                  <!--api限流-->
+                  <div style="margin-top: 20px;">
+                    <p class="lh_15"><span >流量限制&nbsp;:&nbsp;</span>
+                      <el-tooltip class="item" effect="dark" content="网关允许的最大并发数量" placement="top">
+                        <span><i class="el-icon-info"></i></span>
+                      </el-tooltip><br>
+                    </p>
+                    <el-input style="width: 300px;" v-model="RateLimitNum" placeholder="请输入内容"></el-input>
                   </div>
                   <!--api请求重试次数-->
                   <div style="margin-top: 20px;">
@@ -157,8 +178,9 @@
                     <el-tooltip class="item" effect="dark" content="删除" placement="top">
                       <el-popconfirm
                         title="该操作会删除组内所有api，确认删除吗？"
+                        @onConfirm="DeleteApiGroupDetail(scope.row)"
                       >
-                        <el-button @click="DeleteApiGroupDetail" type="danger" slot="reference" icon="el-icon-delete" circle></el-button>
+                        <el-button type="danger" slot="reference" icon="el-icon-delete" circle></el-button>
                       </el-popconfirm>
                     </el-tooltip>
                   </template>
@@ -227,8 +249,9 @@
                     <el-tooltip class="item" effect="dark" content="删除" placement="top">
                       <el-popconfirm
                         title="确认删除吗？"
+                        @onConfirm="DeleteApiDetail(scope.row)"
                       >
-                        <el-button @click="DeleteApiDetail" type="danger" slot="reference" icon="el-icon-delete" circle></el-button>
+                        <el-button type="danger" slot="reference" icon="el-icon-delete" circle></el-button>
                       </el-popconfirm>
                     </el-tooltip>
                   </template>
@@ -287,6 +310,17 @@
         <el-form-item label="api名称" :label-width="formLabelWidth">
           <el-input style="width: 300px;" disabled v-model="ApiDetailsEditForm.ApiName" ></el-input>
         </el-form-item>
+        <!--api服务端协议-->
+        <el-form-item label="服务端协议" :label-width="formLabelWidth">
+          <el-select style="width: 300px;" v-model="ApiDetailsEditForm.ProtocolType">
+            <el-option
+              v-for="item in ProtocolTypes"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
         <el-form-item label="转发地址" :label-width="formLabelWidth">
           <el-input style="width: 500px;" v-model="ApiDetailsEditForm.ApiUrl">
             <template slot="prepend">Http://</template>
@@ -313,6 +347,9 @@
           <el-radio v-model="ApiDetailsEditForm.ApiMethod" label="DELETE">DELETE</el-radio>
           <el-radio v-model="ApiDetailsEditForm.ApiMethod" label="PUT">PUT</el-radio>
           <el-radio v-model="ApiDetailsEditForm.ApiMethod" label="OPTIONS">OPTIONS</el-radio>
+        </el-form-item>
+        <el-form-item label="流量限制" :label-width="formLabelWidth">
+          <el-input style="width: 300px;" v-model="ApiDetailsEditForm.RateLimitNum"></el-input>
         </el-form-item>
         <el-form-item label="超时限制(ms)" :label-width="formLabelWidth">
           <el-input style="width: 300px;" v-model="ApiDetailsEditForm.ApiTimeout"></el-input>
@@ -350,10 +387,12 @@ export default {
         id: '1',
         ApiName: 'test',
         ApiMethod: 'POST',
+        ProtocolType: 'http',
         ApiUrl: 'http://test.com/getApi',
         BackendUrl: '/api/v1/getApis',
         ApiGroup: '112',
         ApiReturnType: 'JSON',
+        RateLimitNum: '2000',
         ApiTimeout: '3000',
         ApiRetry: '3',
         ApiReturnContent: ''
@@ -385,6 +424,13 @@ export default {
         value: '112',
         label: '112'
       }],
+      ProtocolTypes: [{
+        value: 'http',
+        label: 'HTTP'
+      }, {
+        value: 'gRPC',
+        label: 'gRPC'
+      }],
       ReturnTypes: [{
         value: 'RAW',
         label: 'RAW'
@@ -396,12 +442,14 @@ export default {
         label: 'XML'
       }],
       ApiGroup: '',
-      ApiReturnType: '',
+      ApiReturnType: 'JSON',
       ApiUrl: 'test.com/getApis',
       BackendUrl: '/api/v1/getApis',
       ApiName: 'test',
+      ProtocolType: 'http',
       ApiMethod: 'POST',
       ApiTimeout: 3000,
+      RateLimitNum: 2000,
       ApiRetry: 3,
       ApiReturnContent: '',
       formLabelWidth: '120px',
@@ -444,14 +492,17 @@ export default {
       const ApiDetailsCreateForm = {
         'ApiName': this.ApiName,
         'ApiMethod': this.ApiMethod,
+        'ProtocolType': this.ProtocolType,
         'ApiUrl': this.ApiUrl,
         'BackendUrl': this.BackendUrl,
         'ApiGroup': this.ApiGroup,
         'ApiReturnType': this.ApiReturnType,
+        'RateLimitNum': this.RateLimitNum,
         'ApiTimeout': this.ApiTimeout,
         'ApiRetry': this.ApiRetry,
         'ApiReturnContent': this.ApiReturnContent
       }
+      console.log(ApiDetailsCreateForm)
       // 上传表单
       const response = await this.$http.post('/createApiDetail', ApiDetailsCreateForm)
       console.log(response)
@@ -480,17 +531,20 @@ export default {
       this.$message.success('修改api详情成功!')
     },
     // 删除Api详情，提交表单
-    async DeleteApiDetail () {
+    async DeleteApiDetail (row) {
       // 上传表单
-      const response = await this.$http.delete('/deleteApiDetail', this.ApiDetailsEditForm)
+      const response = await this.$http.post('/deleteApiDetail', row)
       console.log(response)
       this.QueryApiDetailList()
       this.$message.success('删除api详情成功!')
     },
     // 删除Api group详情，提交表单
-    async DeleteApiGroupDetail () {
+    async DeleteApiGroupDetail (row) {
       // 上传表单
-      const response = await this.$http.delete('/deleteApiGroupDetail', this.ApiDetailsEditForm)
+      this.ApiGroupDetailEditForm = row
+      console.log(row)
+      console.log(this.ApiGroupDetailEditForm)
+      const response = await this.$http.post('/deleteApiGroupDetail', this.ApiGroupDetailEditForm)
       console.log(response)
       this.QueryApiGroupList()
       this.$message.success('删除api group详情成功!')
