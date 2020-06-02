44790f662718d2280267265f5a2d3e98abfdc960
945188363 <niujingheng1997@163.com>
2020-06-02 23:44:41 +0800

    数据统计多维度展示

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="hello">
-    <div class="line">
-      <button @click="changeType">切换图表类型</button>
-      <ve-chart :data="chartData"
-                :settings="chartSettings"
-                :loading="loading"
-                :data-empty="dataEmpty">
-      </ve-chart>
+    <div class="layer">
+      <div class="line">
+        <!--一周访问统计-->
+        <h4>一周访问统计</h4>
+        <button @click="changeType">切换图表类型</button>
+        <ve-chart :data="chartData"
+                  :settings="chartSettings"
+                  :loading="loading"
+                  :data-empty="dataEmpty">
+        </ve-chart>
+      </div>
+      <div class="line">
+        <h4>服务器性能指标</h4>
+        <ve-ring :data="ringData"></ve-ring>
+      </div>
     </div>
-    <div class="line">
-      <ve-gauge :data="gaugeData"
-                :settings="gaugeSettings"
-                :loading="loading">
-      </ve-gauge>
-    </div>
+    <div class="layer">
+      <div class="line">
+        <!--服务器性能指标-->
+        <h4>服务器性能指标</h4>
+        <ve-gauge :data="gaugeData"
+                  :settings="gaugeSettings"
+                  :loading="loading">
+        </ve-gauge>
+      </div>
+      <div class="line">
+        <!--整体数据展示-->
+        <h4>整体数据展示</h4>
+        <ve-funnel :data="funnelData"></ve-funnel>
 
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +41,10 @@ export default {
   data () {
     this.typeArr = ['line', 'histogram', 'pie']
     this.index = 0
+    this.funnelSettings = {
+      dimension: '状态',
+      metrics: '数值'
+    }
     this.gaugeSettings = {
       dataType: {
         '占比': 'percent'
@@ -38,15 +59,10 @@ export default {
     return {
       dataEmpty: false,
       loading: true,
+      chartSettings: { type: this.typeArr[this.index] },
       chartData: {
         columns: ['日期', '访问用户'],
         rows: [
-          { '日期': '1月1日', '访问用户': 1523 },
-          { '日期': '1月2日', '访问用户': 1223 },
-          { '日期': '1月3日', '访问用户': 2123 },
-          { '日期': '1月4日', '访问用户': 4123 },
-          { '日期': '1月5日', '访问用户': 3123 },
-          { '日期': '1月6日', '访问用户': 7123 }
         ]
       },
       gaugeData: {
@@ -55,7 +71,26 @@ export default {
           { type: '占比', value: 0.8 }
         ]
       },
-      chartSettings: { type: this.typeArr[this.index] }
+      funnelData: {
+        columns: ['状态', '数值'],
+        rows: [
+          { '状态': '展示', '数值': 900 },
+          { '状态': '访问', '数值': 600 },
+          { '状态': '点击', '数值': 300 },
+          { '状态': '订单', '数值': 100 }
+        ]
+      },
+      ringData: {
+        columns: ['日期', '访问用户'],
+        rows: [
+          { '日期': '1/1', '访问用户': 1393 },
+          { '日期': '1/2', '访问用户': 3530 },
+          { '日期': '1/3', '访问用户': 2923 },
+          { '日期': '1/4', '访问用户': 1723 },
+          { '日期': '1/5', '访问用户': 3792 },
+          { '日期': '1/6', '访问用户': 4593 }
+        ]
+      }
     }
   },
   methods: {
@@ -63,7 +98,45 @@ export default {
       this.index++
       if (this.index >= this.typeArr.length) { this.index = 0 }
       this.chartSettings = { type: this.typeArr[this.index] }
+    },
+    async QueryCount () {
+      this.loading = true
+      // 当前日期
+      const endTime = this.getNowFormatDate(1)
+      // 七天前日期
+      const startTime = this.getNowFormatDate(-7)
+      // 拼接参数字符串
+      const param = '?endTime=' + endTime + '&startTime=' + startTime
+      const response = await this.$http.get('/queryCount' + param)
+      console.log(response)
+      let countData = new Map()
+      response.data['data'].forEach((item) => {
+        let time = this.parseDate(item['Time'].slice(0, 10))
+        if (countData.has(time)) {
+          countData.set(time, countData.get(time) + 1)
+        } else {
+          countData.set(time, 1)
+        }
+      })
+      countData.forEach((v, k, map) => {
+        this.chartData.rows.push(
+          {
+            '日期': k, '访问用户': v
+          }
+        )
+      })
+      console.log(this.chartData.rows)
+      this.loading = false
+    },
+    parseDate (date) {
+      let strArr = date.split('-')
+      return strArr[1] + '月' + strArr[2] + '日'
     }
+  },
+  created: function () {
+  },
+  mounted: function () {
+    this.QueryCount()
   }
 }
 </script>
@@ -73,5 +146,13 @@ export default {
   .line{
     width: 500px;
     height: 400px;
+    margin-top: 5px;
+    margin-bottom: 20px;
+  }
+  .layer{
+    margin-top: 5px;
+    margin-bottom: 20px;
+    justify-content: space-around;
+    display: flex;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -22,6 +22,25 @@ Vue.use(VCharts)
 
 Vue.config.productionTip = false
 
+Vue.prototype.getNowFormatDate = function (time) {
+  let date = new Date()
+  // 选择当前时间的几天前或者几天后
+  date.setDate(date.getDate() + time)
+  const year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let strDate = date.getDate()
+  // 补0
+  if (month >= 1 && month <= 9) {
+    month = '0' + month
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = '0' + strDate
+  }
+  // 拼接日期字符串
+  const currentDate = year + '-' + month + '-' + strDate
+  return currentDate
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
